b093f9d0fe30936c48ed37c2bf24404b4b16f889
osgdev <osgdev@DHZ-MeanStk11.wipro.com>
2017-04-05 17:24:11 +0530

    extracting table

--- src/reducers/match.js
+++ src/reducers/match.js
@@ -1,7 +1,17 @@
+import groupBy from 'lodash/groupBy'
+import castArray from 'lodash/castArray'
+import mapKeys from 'lodash/mapKeys'
+import keysIn from 'lodash/keysIn'
+import transform from 'lodash/transform'
+import uniq from 'lodash/uniq'
+import forEach from 'lodash/forEach'
+import findIndex from 'lodash/findIndex'
+import sortBy from 'lodash/sortBy'
+
 const initialState = {
     matchData :[],
     seasonData:{},
-    teamsData :[],
+    tableData :[],
     error:'',
     requestMatchData :false,
     successDataFetch:false
@@ -14,11 +24,54 @@ const reducer = (state = initialState,action) =>{
               requestMatchData : true
             }
         case 'SUCCESS_MATCH_DATA':
+            let {data} = action
+            let seasonData = mapKeys(groupBy(data,'season'),
+                                                (_,key) => 'ipl'+key
+                                                    )
+            let extractedData =   transform(
+                                            seasonData,
+                                            (result,value,key) => {
+                                                let table = uniq(value.map(
+                                                    ({team1}) => team1
+                                                )).map(
+                                                    name => ({ name, played :0,won:0,pts:0})
+                                                )
+                                                forEach(value,
+                                                    ({team1,team2,winner}) =>{
+                                                        let play1 = findIndex(table, (n) => n.name == team1)
+                                                        table[play1] = {...table[play1],played : table[play1].played + 1}
+                                                        let play2 = findIndex(table, (n) => n.name == team2)
+                                                        table[play2] = {...table[play2],played : table[play2].played + 1}
+                                                        let win = findIndex(table, (n) => n.name == winner)
+                                                        if(win !== -1)
+                                                            table[win] = {...table[win],won : table[win].won + 1,pts : table[win].pts + 2}
+                                                        else{
+                                                            table[play1] = {...table[play1],pts : table[play1].pts + 1}
+                                                            table[play2] = {...table[play2],pts : table[play2].pts + 1}
+                                                        }
+
+
+
+                                                    }
+                                                )
+                                                result.push(
+                                                    {
+                                                        key,
+                                                        table
+                                                    }
+                                                )
+                                            },
+                                            []
+                                        )
+
+
             return{
               ...state,
               requestMatchData : false,
               successDataFetch:true,
-              data:action.data
+              seasonData,
+              data,
+              tableData : extractedData.sort((a,b) => a.pts-b.pts)
 
             }
         case 'FAILURE_MATCH_DATA':
