1642190181a265d8e823d4d648a5fa243de764ff
Manoj Kumar <manoj.g.kumar@hotmail.com>
2017-04-08 10:22:14 +0530

    adding search drawyer

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Menu = () =>
+const Menu = ({toggleSeachBox,isOpen}) =>
     <nav className = 'menu'>
       <div className = 'menuItem menuActive'><i className="fa fa-home" aria-hidden="true"></i></div>
       <div className = 'menuItem'><i className="fa fa-pie-chart" aria-hidden="true"></i></div>
-      <div className = 'menuItem'><i className="fa fa-search" aria-hidden="true"></i></div>
+      <div className = {isOpen?'menuItem menuActive':'menuItem'} onClick = {toggleSeachBox} ><i className="fa fa-search" aria-hidden="true"></i></div>
 
     </nav>
 export default Menu
--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -1,22 +1,50 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {Motion,spring,presets} from 'react-motion'
 import css from '../styles/land.styl'
 import Menu from '../components/Menu.jsx'
 import Home from '../components/Home.jsx'
 import { getMatchData } from '../actions'
 
 class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {searchBox:false}
+  }
 
   componentDidMount(){
       this.props.getMatchData()
   }
-
+  toggleSeachBox = () => {
+    this.setState ({searchBox : !this.state.searchBox})
+  }
   render(){
+    let {searchBox} = this.state
     return(
       <div className="container">
-        <Menu/>
+        <Menu toggleSeachBox = {this.toggleSeachBox} isOpen = {this.state.searchBox}/>
         <section className="content">
             <Home/>
+            <Motion
+              style = {{
+                x: spring(searchBox ? 50 : 0,presets.wobbly),
+                opacity : spring(searchBox ? 1:0)
+              }}
+            >
+              {
+                ({x,opacity}) =>
+                <div className="model"
+                  style ={{
+                    width : x+'%',
+                    opacity
+                  }}
+                  >
+
+                </div>
+              }
+
+            </Motion>
+
         </section>
 
       </div>
